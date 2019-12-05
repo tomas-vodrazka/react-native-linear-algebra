c9baf8ef015c08729b08612730873a35972bfc0d
Tomas Vodrazka <tomasvodrazka@W0GL5143.local>
2019-12-05 15:15:58 +0100

    updated sovlers testing

---
+++ src/linearAlgebra/solvers/__tests__/Solvers.test.ts
@@ -0,0 +1,32 @@
+import { TensorFlowSolver } from "../TensorFlowSolver";
+import { NumericSolver } from "../NumericSolver";
+import { getTestCases } from "../../LinearSystemTestCasesProvider";
+
+const solvers = [
+  {
+    name: "TensorFlow",
+    solver: TensorFlowSolver
+  },
+  {
+    name: "Numeric",
+    solver: NumericSolver
+  }
+];
+
+describe("Solvers", () => {
+  describe("solve", () => {
+    getTestCases().forEach((testCase, index) => {
+      solvers.forEach(solver => {
+        it(`Solver ${solver.name} solves test case number #${index +
+          1}`, () => {
+          const actual = solver.solver.solve(testCase.x, testCase.y);
+
+          expect(actual.w.length).toBe(testCase.w.length);
+          actual.w.forEach((val, i) => {
+            expect(val).toBeCloseTo(testCase.w[i]);
+          });
+        });
+      });
+    });
+  });
+});
